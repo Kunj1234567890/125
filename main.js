f0f3265198560f83161175d47f3b37d73681dbbd
Kunj1234567890 <97776880+Kunj1234567890@users.noreply.github.com>
2023-07-14 17:44:59 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+leftWristx = 0;
+rightWristx = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text(':)',50,400);
+}
+
+function modelDone(){
+    console.log("ZE POSENET IS READY TO GO!");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristx = results[0].pose.leftWrist.x;
+        rightWristx = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristx - rightWristx);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
+}
